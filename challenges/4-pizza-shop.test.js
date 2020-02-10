e7e3e258cc32ed6803329481f30355a85ad0557e
Mitch Lloyd <mitch@northcoders.com>
2020-02-10 11:53:20 +0000

    increase timeouts for async tests

--- challenges/4-pizza-shop.test.js
+++ challenges/4-pizza-shop.test.js
@@ -2,12 +2,16 @@ const makePizza = require('./4-pizza-shop.js');
 
 describe('makePizza()', () => {
   test('invokes callback with empty string when passed an empty string', (done) => {
+    jest.setTimeout(3000);
+
     makePizza('', (err, delivery) => {
       expect(delivery).toBe('');
       done();
     });
   });
   test('invokes callback with a hot fresh pizza in a well designed box when passed a single pizza', (done) => {
+    jest.setTimeout(3000);
+
     makePizza('margherita', (err, delivery) => {
       expect(delivery).toBe('a hot margherita in a well designed box');
       done();
