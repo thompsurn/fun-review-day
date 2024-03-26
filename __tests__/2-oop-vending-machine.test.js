dbb07643ae1ec564f66c3aa42e6f5955a5f78b6d
thompsurn <121163412+thompsurn@users.noreply.github.com>
2024-03-26 13:07:27 +0000

    completed oop-vending-machine tasks

--- __tests__/2-oop-vending-machine.test.js
+++ __tests__/2-oop-vending-machine.test.js
@@ -47,4 +47,70 @@ describe("Vending Machine", () => {
       expect(testVendingMachine.creditChecker(100)).toBe(false)
     })
   });
-});
+
+  describe('new vending machine methods', () => {
+    describe('addStock', () => {
+      test('should add new key:value pair of name and stock in the correct position when passed in the position and stock values', () => {
+        const testMachine = new VendingMachine
+        const stock = 'marsBars'
+        const position = 'A'
+  
+        const actual = testMachine.addStock(stock, position)
+        const expected = { 
+        A: { name: 'marsBars' },
+        B: {},
+        C: {} 
+      }
+        expect(actual).toEqual(expected)
+      })
+    })
+      test('should add 2 new key:value pairs of price and quantity in the correct position when passed in the cost and amount values', () => {
+        const testMachine = new VendingMachine
+        const stock = 'marsBars'
+        const position = 'A'
+        const cost = 50
+        const amount = 6
+    
+        const actual = testMachine.addStock(stock, position, cost, amount)
+        const expected = { 
+        A: { name: 'marsBars', price: 50, quantity: 6 },
+        B: {},
+        C: {} 
+        }
+        expect(actual).toEqual(expected)
+      })
+    })
+    describe('purchaseItem', () => {
+      test('should check if the amount of credit in the machine is greater than the item to be purchased and return an error message if it is not', () => {
+        const testMachine = new VendingMachine
+        const marsBar = {name: 'marsBar', price: 50, quantity: 6 }
+
+        testMachine.addStock(marsBar, 'A')
+        testMachine.addCredit(40)
+
+        const actual = testMachine.purchaseItem('A')
+        const expected = 'Insufficient credit!'
+        expect(actual).toBe(expected)
+      })
+      test('should decrease the quantity of the stock if there is sufficient credit and return the name of the product purchased', () => {
+        const testMachine = new VendingMachine
+        const stock = 'marsBar'
+        const position = 'A'
+        const cost = 50
+        const amount = 6
+    
+        testMachine.addStock(stock, position, cost, amount)
+        testMachine.addCredit(60)
+        const actual = testMachine.purchaseItem(position)
+
+        const expected = { 
+        A: { name: 'marsBar', price: 50, quantity: 5 },
+        B: {},
+        C: {} 
+        }
+        expect(actual).toBe('marsBar')
+        expect(testMachine.stock).toEqual(expected)
+      })
+    })
+
+})
--- sections/2-oop-vending-machine.js
+++ sections/2-oop-vending-machine.js
@@ -11,6 +11,26 @@ class VendingMachine {
   creditChecker(price){
     return this.credit >= price ? true : false
   }
+
+  addStock(stock, position, cost, amount) {
+    this.stock[position] = {name: stock, price : cost, quantity: amount}
+    return this.stock
+  }
+
+  purchaseItem(position) {
+    const stock = this.stock[position]
+    
+    if (!this.creditChecker(stock.price)) {
+      return 'Insufficient credit!'
+  }
+
+    stock.quantity--
+    this.credit -= stock.price
+
+    return stock.name
+  }
 }
+
+
 
 module.exports = VendingMachine;
